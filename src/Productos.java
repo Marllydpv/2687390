77b8e51d32056233cf7c42611124cf1aa69486ed
Marllydpv <125328888+Marllydpv@users.noreply.github.com>
2023-02-15 19:04:49 -0500

    Add files via upload

---
+++ src/Productos.java
@@ -0,0 +1,23 @@
+productos
+
+import java.util.Scanner;
+
+public class ####{
+    public static void main(String[] args) {
+        Scanner lectura = new Scanner(System.in);
+        
+        int nProduct, totalPay, pay, change;
+        System.out.println("Ingrese la cantidad de productos comprados: ");
+            nProduct = lectura.nextInt();
+            totalPay = nProduct * 10000;
+        System.out.println("El total a pagar es de $" + totalPay);
+        System.out.println("Ingrese la cantidad de dinero con el que paga: ");
+            pay = lectura.nextInt();
+            change = pay - totalPay;
+        System.out.println("Su cambio será de $" + change);
+
+
+
+        
+        lectura.close();
+}

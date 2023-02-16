dff7193e6060cd4dc3d51a4e4983970a900d02d4
Marllydpv <125328888+Marllydpv@users.noreply.github.com>
2023-02-15 19:04:00 -0500

    Add files via upload

--- src/App.java
+++ src/App.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner lectura = new Scanner(System.in);
+        int number1, number2, resultado;
+        System.out.println("ingrese un numero");
+        number1 = lectura.nextInt();
+        System.out.println("ingrese el otro numero");
+
+        number2 = lectura.nextInt();
+        resultado = lectura.nextInt();
+
+        resultado = number1 + number2;
+
+        System.out.println("la suma entre es" + number1 + " y " + number2 + " es " + resultado);
+        String name;
+        lectura.nextInt();
+        System.out.println("ingrese su nombre");
+
+        name = lectura.nextInt();
+
+        System.out.println("el nombre ingresado fue:" + name);
+
+        lectura.close();
     }
 }

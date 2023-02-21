849cbb37bad833faa2ad1647c4d8442d99b3cfa1
marllydpv <marllydanielapinedavillamil@gmail.com>
2023-02-21 10:50:33 -0500

    probar

---
+++ src/Prueba.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Prueba {
+    public static void main(String[] args) {
+        int precio,cantidad;
+        Scanner lectura = new Scanner(System.in);
+        for (int x=1; x<=5; x++){
+        System.out.println("digite el precio del producto");
+        precio=lectura.nextInt();
+        System.out.println("digite la cantidad del producto");
+        cantidad=lectura.nextInt();
+    }
+    }
+}

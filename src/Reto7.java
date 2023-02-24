4901bc69c732e1757c338b08b46f595560972d11
marllydpv <marllydanielapinedavillamil@gmail.com>
2023-02-24 11:43:02 -0500

    kk

---
+++ src/Reto7.java
@@ -0,0 +1,38 @@
+import java.util.concurrent.ExecutionException;
+
+public class reto7 {
+    public static void main(String[] args) throws ExecutionException {
+        System.out.println(" ingrese el precio de su compra  ");
+        tikect=(int)(Math.random()*10+1)
+        
+
+        int precio,tikect;
+
+            
+
+            if() {
+                System.out.println ("");
+            }
+            else if(){
+                System.out.println ("");
+
+            }
+            else if(){
+                System.out.println (" ");
+
+            }
+            else if(){
+                System.out.println (" ");
+
+            }
+            else if(){
+                System.out.println (" ");
+
+            }
+            else{
+                System.out.println (" ");
+            }
+
+
+    }
+}
